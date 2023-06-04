8f531def7d581390cf29c3f48bc588033b05d39e
Suraj Mukhia <surajmukhia.gcit@rub.edu.bt>
2023-06-04 16:25:55 +0600

    asyncstorge clear

--- App.js
+++ App.js
@@ -1,10 +1,23 @@
 import "./expo-crypto-shim";
 import { StyleSheet, SafeAreaView, StatusBar as SB } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 import MainNavigator from "./navigators/MainNavigator";
 import Colors from "./constants/Colors";
+import { useEffect } from "react";
 
 export default function App() {
+  useEffect(() => {
+    async function clearStorage() {
+      AsyncStorage.clear();
+    }
+    try {
+      clearStorage();
+    } catch (err) {
+      alert(err);
+    }
+  }, []);
+
   return (
     <SafeAreaView style={styles.container}>
       <SB backgroundColor={Colors.secondary} barStyle="default" />
