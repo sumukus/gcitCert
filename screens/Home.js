4a4be1edf866c8f74840348a2a198ee856d17fdf
Suraj Mukhia <surajmukhia.gcit@rub.edu.bt>
2023-06-05 19:33:09 +0600

    modified files

--- screens/Home.js
+++ screens/Home.js
@@ -1,35 +1,16 @@
-import { Text, View, StyleSheet, Pressable } from "react-native";
+import { Text, Button, View, StyleSheet } from "react-native";
 import Colors from "../constants/Colors";
 import VerifyCert from "../components/VerifyCert";
-import { Web3Modal, Web3Button, useWeb3Modal } from "@web3modal/react-native";
 import { PROJECT_ID } from "@env";
 
 function Home({ navigation }) {
-  //walletconnect code
-  const projectId = PROJECT_ID;
-
-  const { open, isConnected, provider, address } = useWeb3Modal();
-
-  const providerMetadata = {
-    name: "gcitCert dApp",
-    description: "A dApp for issuing the certificates by GCIT college",
-    url: "https://www.gcit.edu.bt",
-    icons: [
-      "https://www.gcit.edu.bt/wp-content/uploads/sites/4/2023/03/GCIT_Logo.png",
-    ],
-  };
-
   return (
     <View style={styles.rootContainer}>
       <View style={styles.verifyCert}>
         <VerifyCert />
       </View>
       <View style={styles.loginContainer}>
-        {/* <Pressable onPress={open}>
-          <Text>{isConnected ? "View Account" : "Connect"}</Text>
-        </Pressable> */}
-        <Web3Button />
-        <Web3Modal projectId={projectId} providerMetadata={providerMetadata} />
+        <Button title="Connect Metamask" color={Colors.secondary} />
       </View>
     </View>
   );
